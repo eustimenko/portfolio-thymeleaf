cd372e9cbaa365d4b847c88bb1e5137c2dcc0022
Eugene Ustimenko <evgenyustimenko@gmail.com>
2017-07-21 19:13:18 +0700

    Test exception handling

--- src/test/java/com/eustimenko/portfolio/thymeleaf/controller/UserControllerTest.java
+++ src/test/java/com/eustimenko/portfolio/thymeleaf/controller/UserControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -19,6 +20,9 @@ public class UserControllerTest {
 
     @Autowired
     private MockMvc mvc;
+
+    @Autowired
+    private JdbcTemplate jdbc;
 
     @Test
     public void getUsersView() throws Exception {
@@ -51,6 +55,14 @@ public class UserControllerTest {
     @Test
     public void pageNotFound() throws Exception {
         mvc.perform(get("/abc"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void errorDueToWork() throws Exception {
+        jdbc.setDataSource(null);
+
+        mvc.perform(get("/"))
                 .andExpect(status().isNotFound());
     }
 }
